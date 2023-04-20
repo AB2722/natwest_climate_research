8e847307dd2d2514bc3361ca8c6c322c8ce278ed
ab2722 <ab2722@ic.ac.uk>
2023-04-20 20:41:24 +0100

    Pairplot of data, scatter of data, trendline, coeffs calculated

--- natwest_data_analysis.py
+++ natwest_data_analysis.py
@@ -3,44 +3,86 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tic
 import numpy as np
+from scipy.stats import pearsonr
+from scipy.stats import spearmanr
 
 #Reading in the csv file containing the data. 
 df = pd.read_csv("natwest_data.csv")
 
-#Creating figure and subplots.
-fix, ax = plt.subplots()
+#Exctract the correctn years and the change in deaths and temp.
+change_in_temp = []
+change_in_deaths = []
+years = []
 
-#Using twinx to plot both mean temp and malaria deaths on same plot.
-ax.plot(df["year"],
-        df["mean_temp"],
+for i in range(1,len(df["malaria_deaths"])-1):
+    change_in_deaths.append(df["malaria_deaths"][i] - df["malaria_deaths"][i-1])
+    change_in_temp.append(df["mean_temp"][i] - df["mean_temp"][i-1])
+    years.append(df["year"][i])
+
+#Creating figure and subplots.
+fig, axs = plt.subplots(2)
+
+#Using twinx to plot both change in mean temp and change in malaria deaths on same plot.
+axs[0].plot(years,
+        change_in_temp,
         color="red",
         marker="o",
         markersize=3
         )
 
-#Setting up mean temp plot.
-ax.set_xlabel("Year", fontsize = 14)
-ax.set_ylabel("The Mean Temperature",
+#Setting up change in mean temp plot.
+axs[0].set_xlabel("Year", fontsize = 14)
+axs[0].set_ylabel("Change In Mean Temperature",
               color="red",
               fontsize=14)
 
-ax2 = ax.twinx()
+ax2 = axs[0].twinx()
 
-#Setting up malaira deaths plot
-ax2.plot(df["year"],
-         df["malaria_deaths"],
+#Setting up change in malaira deaths plot.
+ax2.plot(years,
+         change_in_deaths,
          color="blue",
          marker="o",
          markersize=3
         )
 
-ax2.set_ylabel("Malaria Deaths", color="blue", fontsize=14)
+ax2.set_ylabel("Change In Malaria Deaths", color="blue", fontsize=14)
 
 #Some cleaning up of the graph and ensuring x axis has integer years.
-ax.margins(0.05)
-ax.axis("tight")
-fix.tight_layout
-ax.xaxis.set_major_locator(tic.MaxNLocator(integer=True))
+axs[0].margins(0.05)
+axs[0].axis("tight")
+fig.tight_layout
+axs[0].xaxis.set_major_locator(tic.MaxNLocator(integer=True))
+axs[0].grid("on")
+axs[0].set_title("The changes in Malaria deaths and Mean temperature in Nigeria through 2001 - 2019")
+
+#plotting change in deaths against change in temp.
+axs[1].plot(
+    change_in_deaths,
+    change_in_temp,
+    "o",
+    color="blue"
+)
+
+#Adding some axis labels.
+axs[1].set_xlabel("Change In Malaria Deaths", fontsize=14)
+axs[1].set_ylabel("Change In Mean Temperature", fontsize=14)
+axs[1].grid("on")
+axs[1].set_title("The changes in Malaria deaths against the changes in Mean temperature in Nigeria through 2001 - 2019")
+
+#creating function for trendline.
+z = np.polyfit(change_in_deaths, change_in_temp, 1)
+p = np.poly1d(z)
+
+axs[1].plot(
+    change_in_deaths,
+    p(change_in_deaths),
+    color="blue",
+    linewidth=1)
+
+#Calculating spearmans and pearsons correlation coefficient on the data.
+pcorr, _ = pearsonr(change_in_deaths, change_in_temp)
+scorr, _ = spearmanr(change_in_deaths, change_in_temp)
 
 #Plotting graph
 plt.show()
